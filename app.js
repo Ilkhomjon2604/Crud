746e249aa4a8eddc86a3ec31cfe637a6a09c5054
Ilkhomjon2604 <m347es@gmail.com>
2023-07-18 11:36:42 +0200

    crud

---
+++ app.js
@@ -0,0 +1,16 @@
+let cars = [
+    {id:1 , year:1997 , engine : 1, name: 'Tico'},
+    {id:2 , year:2005 , engine : 1.2, name: 'Damas'},
+    {id:3 , year:2018 , engine : 1.5, name: 'Tracker'},
+    {id:4 , year:2010 , engine : 1.2, name: 'Lacetti'},
+    {id:5 , year:2012 , engine : 1.9, name: 'Malibu'},
+    {id:6 , year:2022 , engine : 1.6, name: 'Tesla'},
+    {id:7 , year:2009 , engine : 2, name: 'Matzi'},
+
+]
+
+
+// CRUD => CREAT , READ , UPDATE , DELETE 
+
+// Methods : Map(), fill(), ForEach(),  flat(), sort(), filter(), reduce(), every(), some(), copyWith(), Array.from(), newSet(), new Map(), flatMap(); 
+
